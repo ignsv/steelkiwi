afe4138daa950e69e8cc06622570f4305eda0520
Ignatenko <krylov1295@gmail.com>
2016-10-25 01:13:04 +0300

    add base template and handle index page through the index view

--- kiwisite/product/views.py
+++ kiwisite/product/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'product/index.html')
+
+def products(request):
+    pass
+
+def categories(request):
+    pass
+
+def product(request):
+    pass
+
+def secret(request):
+    pass
