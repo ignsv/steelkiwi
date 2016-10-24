381ea7ff44dd0a9c3379e61bc3a3873037a414b2
Ignatenko <krylov1295@gmail.com>
2016-10-24 23:50:34 +0300

    add models and create first migrate

--- kiwisite/product/models.py
+++ kiwisite/product/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super(Category, self).save(*args, **kwargs)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    created_at = models.DateTimeField("creation date")
+    modified_at = models.DateTimeField("modification date")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super(Product, self).save(*args, **kwargs)
+
+
+
+
