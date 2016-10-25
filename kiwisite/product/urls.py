15a3db92216a803dfb3611851259f3acf2d7622d
Ignatenko <krylov1295@gmail.com>
2016-10-25 18:46:09 +0300

    add /products/ template and view

--- kiwisite/product/urls.py
+++ kiwisite/product/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'product'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^products/$', views.categories, name='categories'),
 ]
--- kiwisite/product/views.py
+++ kiwisite/product/views.py
@@ -3,16 +3,24 @@ from django.http import Http404
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 
+from .models import Category, Product
+
 # Create your views here.
 
 def index(request):
     return render(request, 'product/index.html')
 
+
 def products(request):
     pass
 
+
 def categories(request):
-    pass
+    category_list = Category.objects.all()
+    context = {
+        'category_list': category_list,
+    }
+    return render(request, 'product/categories.html', context)
 
 def product(request):
     pass
