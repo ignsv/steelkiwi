4110474ce1f52502d68510618c9cb681b0707644
Ignatenko <krylov1295@gmail.com>
2016-10-26 03:30:36 +0300

    improve unit test. Chech date validation

--- kiwisite/product/tests.py
+++ kiwisite/product/tests.py
@@ -7,7 +7,6 @@ from django.utils import timezone
 import datetime
 
 # Create your tests here.
-
 class SecretPageTest(TestCase):
 
     def test_non_authenticated_user(self):
@@ -20,24 +19,21 @@ class SecretPageTest(TestCase):
         response = self.client.get(reverse('product:secret'), follow=True)
         self.assertEqual(response.status_code, 200)
 
-    # def test_date_validation(self):
-    #     User.objects.create_user('temporary', 'temporary@example.com', "temporary")
-    #     category = Category.objects.create('category', 'slug_category', "desc")
-    #     set_all = category.product_set.all()
-    #     product = Product.objects.create(
-    #         name = 'Trully',
-    #         price = 15.5,
-    #         created_at=timezone.now()-datetime.timedelta(hours=12),
-    #         modified_at = timezone.now() - datetime.timedelta(hours=5),
-    #         slug = "slugy",
-    #         description = 'desc',
-    #     )
-    #
-    #     self.client.login(username='temporary', password ='temporary')
-    #     response = self.client.get(reverse('product:secret'), follow=True)
-    #
-    #     #user get into secret page
-    #     self.assertContains(response, "that created in range 24 hours")
-    #     #self.assertQuerysetEqual(response.context['product_list'], ['<Product: Trully>'])
+    def test_date_validation(self):
+        #add User
+        User.objects.create_user('temporary', 'temporary@example.com', "temporary")
+        category = Category.objects.create(name = 'category', slug='slug_category',description="desc")
+        category.product_set.create(name= 'Trully', slug = "slugy", description='desc',price= 15.5,
+                                    created_at=timezone.now()-datetime.timedelta(hours=12),
+                                    modified_at=timezone.now() - datetime.timedelta(hours=5)
+                                    )
+        #login user
+        self.client.login(username='temporary', password ='temporary')
+        response = self.client.get(reverse('product:secret'), follow=True)
+
+        #user get into secret page
+        self.assertContains(response, "that created in range 24 hours")
+        #check if product in range 24 hours from now is added to secret page
+        self.assertQuerysetEqual(response.context['product_list'], ['<Product: Trully>'])
 
 
